23174ea267ec8515bfa3eda2a6d900b90f3fc755
Mubtasim Sawonto <mubtasimsawonto@Mubtasims-MBP.fios-router.home>
2020-09-22 20:12:42 -0400

    final

--- Texting.py
+++ Texting.py
@@ -1,3 +1,3 @@
 import boto3
 client= boto3.client ('sns', 'us-east-1')
-client.publish(PhoneNumber= '+13476573589', Message= '#BlackLivesMatter')
+client.publish(PhoneNumber= '+1234567890', Message= '#BlackLivesMatter')
